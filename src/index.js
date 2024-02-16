e62c301b38704b69503128c6007a71fb42a0b342
Akshay Patel <akshaypatel.ap1320@gmail.com>
2024-02-16 13:03:19 +0530

    app.js implemented with express

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import connectWithDb from "./db/connect.js";
 import "dotenv/config.js";
 
+import connectWithDb from "./db/connect.js";
 connectWithDb();
+
+import { app } from "./app.js";
+
+const PORT = process.env.PORT || 3030;
+const server = app.listen(PORT, () => {
+  console.log("Listening on port:", PORT);
+});
+
+server.on("error", (error) => {
+  console.log("Error: ", error.message);
+  process.exit(1);
+});
