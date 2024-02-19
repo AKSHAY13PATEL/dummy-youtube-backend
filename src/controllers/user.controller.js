bc755e0038d37110a1d84cbc11d62b8cedf11b5d
Akshay Patel <akshaypatel.ap1320@gmail.com>
2024-02-19 22:30:22 +0530

    ADD: register user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,59 @@
-export const registerUser = (req, res) => {
-  res.send("User registered");
+import { User } from "../models/user.model.js";
+import { uploadFile } from "../utils/cloudinary.js";
+
+export const registerUser = async (req, res) => {
+  const { username, email, fullName, password } = req.body;
+
+  let data = [username, email, fullName, password];
+  if (data.some((e) => e === "")) {
+    return res.status(400).send("Fields can not be empty");
+  }
+
+  let existingUser = await User.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+  if (existingUser) {
+    return res.status(400).send("user already exist");
+  }
+
+  const localAvatarPath = req.files.avatar && req.files.avatar[0]?.path;
+  const localCoverPath = req.files.coverImage && req.files.coverImage[0]?.path;
+
+  console.log(req.files);
+  console.log("multer avatar :", localAvatarPath);
+  console.log("multer cover :", localCoverPath);
+
+  if (!localAvatarPath) {
+    return res.status(400).send("Avatar is required");
+  }
+
+  // fix name to uploadToCloudinary and modify it with return of null
+  const avatar = await uploadFile(localAvatarPath);
+  const coverImage = await uploadFile(localCoverPath);
+
+  console.log("cloudianry avatar :", avatar);
+  console.log("cloudianry cover :", coverImage);
+
+  if (!avatar) {
+    return res.status(500).send("Could not upload avatar");
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    fullName,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const newUser = await User.findById(user._id).select({
+    password: 0,
+    refreshToken: 0,
+  });
+
+  if (!newUser) {
+    return res.status(500).send("something went wrong while registering user");
+  }
+  return res.status(201).send(newUser);
 };
