110891777a738bb075c8951b496116bad9c8f8c7
Akshay Patel <akshaypatel.ap1320@gmail.com>
2024-04-09 16:14:25 +0530

    Catching exception outside of req processing pipeline

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import "dotenv/config.js";
 import connectWithDb from "./db/connect.js";
 connectWithDb();
 
+process.on("uncaughtException", (ex) => {
+  console.log("Uncaught exception occured!");
+});
+
+process.on("unhandledRejection", (ex) => {
+  console.log("Unhandles rejection occured!");
+});
+
 import { app } from "./app.js";
 
 const PORT = process.env.PORT || 3030;
